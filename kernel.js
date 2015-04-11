033184fdf6ce3f1ea200e21df332a4b43530ddf2
John McLear <john@mclear.co.uk>
2015-04-11 11:31:27 +0100

    blabla

--- kernel.js
+++ kernel.js
@@ -508,6 +508,13 @@
 
     // HACK TODO
     // This is completely the wrong way to do it but for now it shows it works
+    if(path == "underscore"){
+      // console.warn("path is async and we're doing a ghetto fix");
+      path = "underscore/underscore";
+    }
+
+    // HACK TODO
+    // This is completely the wrong way to do it but for now it shows it works
     if(path == "unorm"){
       // console.warn("path is async and we're doing a ghetto fix");
       path = "unorm/lib/unorm";
